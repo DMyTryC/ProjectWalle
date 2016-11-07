7be44584141071ffbc3da75ab10b93c5fd0db2ab
DMyTryC <dymytry999@yahoo.com>
2016-11-07 15:46:49 +0100

    Add files via upload

--- src/ev3dev/java/walle/Grabber.java
+++ src/ev3dev/java/walle/Grabber.java
@@ -6,42 +6,56 @@ import ev3dev.hardware.motor.EV3MediumRegulatedMotor;
 public class Grabber {
 
 	private EV3MediumRegulatedMotor gm;
-	private static final int SPEED = 400;
-	private static final int DELAY = 500;
+	private static final int SPEED = 700;
+	private static final int DELAY = 200;
+	private static final int RAMP = 500;
+	
 
 	public Grabber(String sensorPort) {
 		gm = new EV3MediumRegulatedMotor(sensorPort);
-
-		System.out.println("run");
-		System.out.println("speed1 : " + gm.getSpeed());
-		Delay.msDelay(DELAY);
-		gm.setSpeed(SPEED);
-		System.out.println("speed2 : " + gm.getSpeed());
+		this.init();
 	}
 
-	public void open() {
-		gm.forward();
+	private void set() {
 		Delay.msDelay(DELAY);
-		while (!(gm.isStalled())) {
+		System.out.println("Speed = " + gm.getSpeed());
+		gm.setSpeed(SPEED);
+		gm.setRampUp(RAMP);
+		gm.setRampDown(RAMP);
+		System.out.println("Speed has been set to " + gm.getSpeed());
+		
+	}
+	private void init() {
+		
+		set();
+		System.out.println("INIT...");
+	    
+		do {
 			gm.forward();
-		}
+			System.out.print("\r"+gm.isStalled());
+		} while (!(gm.isStalled()));
+	    System.out.println("\nSTALLED.");
+		
+	    gm.resetTachoCount();
+		System.out.println("RESET COUNT. Position = " + gm.getTachoCount());
+		
+		set();
+		open();
 	}
 
 	public void test() {
-		this.open();
-		Delay.msDelay(DELAY);
-		this.close(3400);
+		this.close();
 	}
-
-	public void run() {
-		
+	
+	public void open(){
+		System.out.println("OPENNING...");
+		gm.rotateTo(-1800);
+		System.out.println("OPENED. Position = " + gm.getTachoCount());
 	}
-
-	public void open(int angle) {
-		gm.rotate(angle);
-	}
-
-	public void close(int angle) {
-		gm.rotate(-angle);
+	
+	public void close(){
+		System.out.println("CLOSING...");
+		gm.rotateTo(-3400);
+		System.out.println("CLOSED. Position = " + gm.getTachoCount());
 	}
 }

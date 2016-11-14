24812a4c0401c67f350f08097a2a4808ad298d31
DMyTryC <dymytry999@yahoo.com>
2016-11-14 21:03:46 +0100

    Add files via upload

--- src/ev3dev/java/walle/main/MainGrabber.java
+++ src/ev3dev/java/walle/main/MainGrabber.java
@@ -1,13 +1,12 @@
 package ev3dev.java.walle.main;
 
 import ev3dev.hardware.port.MotorPort;
-import ev3dev.java.walle.Grabber;
+import ev3dev.java.walle.actuators.Grabber;
 
 public class MainGrabber {
 	public static void main(String[] args){
 		Grabber g = new Grabber(MotorPort.D);
-		
-		g.test();
+		g.close();
 	}
 }
  

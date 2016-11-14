28b2174efc3120da1f1e0107091458c2cc85752f
DMyTryC <dymytry999@yahoo.com>
2016-11-14 21:04:58 +0100

    Add files via upload

--- src/ev3dev/java/walle/main/MainCherchePallet.java
+++ src/ev3dev/java/walle/main/MainCherchePallet.java
@@ -1,10 +1,12 @@
 package ev3dev.java.walle.main;
 
+import ev3dev.hardware.port.MotorPort;
+import ev3dev.hardware.port.SensorPort;
 import ev3dev.java.walle.CherchePallet;
 
 public class MainCherchePallet {
 	public static void main(String[] args) {
-		CherchePallet c = new CherchePallet();
+		CherchePallet c = new CherchePallet(MotorPort.B, MotorPort.C,SensorPort.S2, SensorPort.S1,MotorPort.D);
 		
 		c.run();
 	}

a76302a1714b75d67427e6428b633638dc80be93
Mathieu Battier <mayt38@gmail.com>
2016-11-21 15:07:47 +0100

    Nouvelle organisation générale du code
    J'ai refait toute l'organisation du code. Il faut encore recopier quelques parties et les adapter à la nouvelle organisation (cherchepalet, le capteur de distance, etc.), du coup je met tout dans un nouvelle branche et on écrasera l'ancien code que quand on aura tout fait comme il faut.

---
+++ src/ev3dev/java/walle/WallE.java
@@ -0,0 +1,14 @@
+package ev3dev.java.walle;
+
+import ev3dev.java.walle.robot.Robot;
+
+public class WallE {
+
+	
+	public static void main(String[] args) {
+		Robot r = new Robot();
+		
+		r.variousTest();
+	}
+
+}

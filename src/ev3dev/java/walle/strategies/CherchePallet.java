162c5a0d6251df9fdce77d02988273719cd72ce9
DMyTryC <dymytry999@yahoo.com>
2016-12-06 17:02:39 +0100

    Add files via upload

--- src/ev3dev/java/walle/strategies/CherchePallet.java
+++ src/ev3dev/java/walle/strategies/CherchePallet.java
@@ -7,6 +7,27 @@ import ev3dev.java.walle.actuators.DifferentialDrive;
 import ev3dev.java.walle.actuators.Grabber;
 import ev3dev.java.walle.sensors.TouchSensor;
 
+/**
+ * Strategie qui permet de rechercher un palet en tournant sur soit meme
+ * 
+ * Cette strategie consistera en tourner 8 fois a un angle de 45 et si
+ * le robot trouve un objet a une distance entre 250 et (450 + dist_old)
+ * il va retourner en arriere en faisant un narrowSearch() du pallet ou du mur
+ * pour trouver la position exacte du objet trouve. Apres avoir trouve un pallet 
+ * ou un mur, on va avancer vers l'objet et si c'est un mur, alors on va s'arreter et
+ * on va aller vers une autre place et recommencer la recherche
+ * 
+ * @member min
+ * 			le minimum de distance qu'on trouvera a chaque fois qu'on tourne
+ * @member ERROR
+ * 			l'erreur utilise pendant la recherche du pallet quand on retourne en arriere
+ * @member ERROR2
+ * 			l'erreur utilise pendant la narrowSearch() d'un pallet
+ * @member found
+ * 			une variable qui sera initialise a true si le robot trouve un pallet ou un mur
+ * @member dist_old
+ * 			une variable qui sera utilise pour augmenter la distance maximum de recherche par 100 apres chaque tour
+ */
 public class CherchePallet {
 
 	private DifferentialDrive dd;
@@ -15,12 +36,23 @@ public class CherchePallet {
 	private Grabber g;
 
 	private float min;
-	private float dist = 0;
 	private static final float ERROR = 30;
 	private static final float ERROR2 = 20;
 	private boolean found = false;
 	private float dist_old = 0;
 
+	/**
+	 * Constructeur qui sera utilise pour initialiser les parties hardware
+	 * 
+	 * @param dd
+	 * 			les moteurs
+	 * @param us
+	 * 			le distance sensor
+	 * @param ts
+	 * 			le touch sensor
+	 * @param g
+	 * 			le grabber
+	 */
 	public CherchePallet(DifferentialDrive dd, EV3UltrasonicSensor us,
 			TouchSensor ts, Grabber g) {
 		this.dd = dd;
@@ -28,9 +60,13 @@ public class CherchePallet {
 		this.ts = ts;
 		this.g = g;
 	}
-
+	
+	/**
+	 * La methode principale qui va fermer les pinces et qui apres va chercher
+	 * un objet jusqu'au moment ou il trouve quelque chose
+	 * Si on trouve un objet on va avancer vers lui
+	 */
 	public void run() {
-		g.init();
 		g.close();
 
 		while (!found) {
@@ -40,17 +76,29 @@ public class CherchePallet {
 			}
 		}
 	}
-
+	
+	/**
+	 * La methode qui va chercher un objet
+	 * 
+	 * On va avoir un minimumFilter qui prendra des samples pendant que le robot tourne
+	 * Le robot va tourner 8 fois a 45 degrees. En temps que le robot tourne on va prendre une valeur min
+	 * Le min sera initialise a la valeur de sample[0] qui sera le min de l'echantillon
+	 * Si le minimum sera entre 250 et (450 + dist_old) on va tourner en arriere pour trouver
+	 * la position minimum avec une erreur de +- 30 mm ou on trouvera forcement le plus proche objet
+	 * dans l'echantillon pris dans les 45 degrees
+	 * Quand on trouvera l'objet on fera un narrowSearch() pour etre exactement en face du objet
+	 * 
+	 * Si on ne trouve pas un objet entre 250 et (450 + dist_old) on va augmenter la dist_old par 100
+	 * pour pouvoir rechercher des objets plus loin 
+	 */
 	private void cherche() {
-
+		float dist = 0;
 		SampleProvider distance = us.getDistanceMode();
 		SampleProvider min_sample = new MinimumFilter(distance, 5);
 		float[] sample = new float[min_sample.sampleSize()];
 
 		for (int i = 0; i < 8; i++) {
 			dd.rotateTo(-97, true);
-
-			System.out.println(dd.isMoving());
 
 			while (dd.isMoving()) {
 				min_sample.fetchSample(sample, 0);
@@ -81,6 +129,10 @@ public class CherchePallet {
 		dist_old += 100;
 	}
 
+	/**
+	 * Methode qui permet de faire une recherche plus precise de la position
+	 * du objet trouve pour pouvoir trouver la position exacte (+- erreur de 20 mm) du objet
+	 */
 	private void narrowSearch() {
 		dd.changeSpeed(50, 200);
 		dd.rotateTo(-15, true);
@@ -97,6 +149,12 @@ public class CherchePallet {
 		}
 		dd.changeSpeed(500, 500);
 	}
+	
+	/**
+	 * Methode qui sera utilise pour avancer vers un objet trouve
+	 * Si la distance au objet sera plus petite de 20 cm, on va aller en arriere,
+	 * tourner a 70 degrees et avancer pour pouvoir refaire la recherche
+	 */
 	private void avance() {
 		SampleProvider distance = us.getDistanceMode();
 		float[] sample = new float[distance.sampleSize()];
--- src/ev3dev/java/walle/strategies/Strat1.java
+++ src/ev3dev/java/walle/strategies/Strat1.java
@@ -1,23 +1,38 @@
 package ev3dev.java.walle.strategies;
 
 import lejos.robotics.SampleProvider;
-import ev3dev.hardware.port.MotorPort;
-import ev3dev.hardware.port.SensorPort;
 import ev3dev.hardware.sensor.ev3.EV3UltrasonicSensor;
 import ev3dev.java.walle.actuators.DifferentialDrive;
 import ev3dev.java.walle.actuators.Grabber;
-import ev3dev.java.walle.sensors.ColorSensor;
 import ev3dev.java.walle.sensors.TouchSensor;
-import ev3dev.java.walle.utils.State;
 
+/**
+ * Une classe qui sera apellee au debut du match pour mettre le premier pallet dans
+ * la zone du but
+ *
+ * @member degaj
+ * 			variable utilise pour aller autour du deuxieme pallet dans la premiere ligne
+ */
 public class Strat1 {
-	private State s;
 	private DifferentialDrive dd;
 	private TouchSensor ts;
 	private EV3UltrasonicSensor us;
 	private Grabber g;
+	
 	private boolean degaj = false;
-
+	
+	/**
+	 * Constructeur qui sera utilise pour initialiser les parties hardware
+	 * 
+	 * @param dd
+	 * 			les moteurs
+	 * @param us
+	 * 			le distance sensor
+	 * @param ts
+	 * 			le touch sensor
+	 * @param g
+	 * 			le grabber
+	 */
 	public Strat1(DifferentialDrive dd, TouchSensor ts, Grabber g, EV3UltrasonicSensor us) {
 		this.dd = dd;
 		this.ts = ts;
@@ -25,6 +40,11 @@ public class Strat1 {
 		this.us = us;
 	}
 
+	/**
+	 * Methode utilise pour mettre le premier pallet dans la zone but
+	 * On va avancer et prendre le premier pallet apres on va eviter le deuxieme pallet
+	 * et on va aller vers la zone du but pour mettre le pallet dans la zone.
+	 */
 	public void run() {
 		dd.changeSpeed(500, 1500);
 		g.open();
